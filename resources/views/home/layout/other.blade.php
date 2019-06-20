d3f0637c3a77b94816b4fa2f0b3b56e8ec38b817
sundadi666 <539998942@qq.com>
2019-06-20 12:09:37 +0800

    2019年6月20日12:09:28 修改前台模板

--- resources/views/home/layout/other.blade.php
+++ resources/views/home/layout/other.blade.php
@@ -12,9 +12,21 @@
 
 		<link href="/h/css/personal.css" rel="stylesheet" type="text/css">
 		<link href="/h/css/orstyle.css" rel="stylesheet" type="text/css">
+		
+		<link href="https://cdn.bootcss.com/twitter-bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
 
 		<script src="/h/AmazeUI-2.4.2/assets/js/jquery.min.js"></script>
+		<script src="https://cdn.bootcss.com/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<script src="/h/AmazeUI-2.4.2/assets/js/amazeui.js"></script>
+		<link rel="stylesheet" href="/layui/css/layui.css">
+      	<script src="/layui/layui.js"></script>
+		
+		<script>
+		//一般直接写在一个js文件中
+		    layui.use(['layer', 'form'], function(){
+		      var layer = layui.layer
+		    });
+		</script> 
 
 	</head>
 
