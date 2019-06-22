e44a859d1dc03517385c4e42d1928c6e554edf60
lxy88888888 <liuxiaoyan88888888@163.com>
2019-06-22 20:16:32 +0800

    2019-06-22 20:16

--- app/Http/Controllers/Home/GoodsController.php
+++ app/Http/Controllers/Home/GoodsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Home;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Goods;
+use App\Models\Sizes;
 
 class GoodsController extends Controller
 {
@@ -16,8 +17,25 @@ class GoodsController extends Controller
     	$gid = $request->input('gid');
     	//
     	$goods = Goods::find($gid);
+    	
+    	// dd($goods);
 
     	return view('home.goods.details',['goods'=>$goods]);
 
     }	
+
+    // 前台 商品 大小
+    public function getSize(Request $request)
+    {
+    	// 接收 型号 id
+    	$id = $request->input('id');
+    	// 所有商品 大小
+    	$sizes = Sizes::where('mid',$id)->get();
+
+    	if($sizes) {
+    		echo json_encode($sizes);
+    	} else {
+    		echo json_encode(['msg'=>'该商品没有大小']);
+    	}
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -71,21 +71,11 @@
 
 
 
-// 友情链接
-Route::resource('admin/links','Admin\LinksController');
 
-// 导航
-Route::resource('admin/navicates','Admin\NavigatesController');
 
-// 分类
-Route::resource('admin/cates','Admin\CatesController');
 
-// 品牌
-Route::resource('admin/brands','Admin\BrandsController');
 
 
-// 订单
-Route::resource('admin/orders','Admin\OrdersController');
 
 
 
@@ -158,45 +148,24 @@ Route::resource('admin/orders','Admin\OrdersController');
 
 
 
-
-
-
-
-// 公告列表 路由
-Route::resource('admin/news','Admin\NewsController');
-
-// 商品列表 路由
-Route::resource('admin/goods','Admin\GoodsController');
-
-// 商品推荐 路由
-Route::get('admin/goods/setRecommend/{id}','Admin\GoodsController@setRecommend');
-
-// 商品型号添加 路由
-Route::resource('admin/models','Admin\ModelsController');
-
-//轮播图 路由
-Route::resource('admin/banners','Admin\BannersController');
-
-// 商品大小显示页面 路由
-Route::get('admin/sizes/create/{id}','Admin\SizesController@create');
-
-// 商品大小执行添加 路由
-Route::post('admin/sizes/store','Admin\SizesController@store');
 
 // 前台用户留言表单 路由
 Route::get('home/addmsg','Home\ReplysController@create');
 
-// 后台用户留言列表 显示 路由
-Route::get('admin/replys/index','Admin\ReplysController@index');
 
 // 前台用户提交留言 路由
 Route::resource('home/replys','Home\ReplysController');
 
-// 后台查看用户留言详情 路由
-Route::get('admin/replys/{id}','Admin\ReplysController@show');
 
-// 后台查看用户留言详情 路由
-Route::get('admin/replys','Admin\ReplysController@index');
+// 前台 用户 收货地址管理
+Route::resource('home/addrs','Home\AddrsController');
+
+
+
+
+
+
+
 
 // 前台列表页 查询 数据 路由
 Route::get('home/list','Home\ListController@index');
@@ -207,11 +176,8 @@ Route::get('home/list/index','Home\ListController@index');
 // 前台商品详情页 显示 路由
 Route::get('home/goods/details','Home\GoodsController@index');
 
-// 后台商品属性 添加页面 路由
-Route::get('admin/attributes/create/{id}','Admin\AttributesController@create');
-
-// 后台商品属性 执行添加 路由
-Route::post('admin/attributes/store/{id}','Admin\AttributesController@store');
+// 前台获取 商品 大小
+Route::get('home/goods/getsize','Home\GoodsController@getsize');
 
 
 
@@ -225,18 +191,33 @@ Route::post('admin/attributes/store/{id}','Admin\AttributesController@store');
 
 
 
+// 前台 加入购物车 路由
+Route::get('home/carts/addcart','Home\CartsController@addCart');
 
+// 前台 购物车路由
+Route::resource('home/carts','Home\CartsController');
 
+// 前台 修改密码页面 路由
+Route::get('home/personal/upass','Home\PersonalController@upass');
+// 前台 修改密码 方法
+Route::post('home/personal/updata_upass/{id}','Home\PersonalController@updata_upass');
 // 前台 个人资料 路由
 Route::get('home/personal/info/{id}','Home\PersonalController@personalInfo');
 // 前台 个人中心 路由
 Route::resource('home/personal','Home\PersonalController');
 
 
+
+
 // 前台 登陆 路由
 Route::resource('home/login','Home\LoginController');
+
+// 前台 退出 路由
+Route::get('home/logout','Home\LoginController@logout');
+
 // 前台 登陆验证路由
 Route::post('home/login/login','Home\LoginController@login');
+
 // 前台 激活 用户(邮箱) 路由
 Route::get('home/register/chengeStatus/{id}/{token}','Home\RegisterController@chengeStatus');
 
@@ -249,22 +230,99 @@ Route::post('home/register/phone','Home\RegisterController@phone');
 // 前台 手机 邮箱 注册路由
 Route::resource('home/register','Home\RegisterController');
 
+
+
 // 后台 登陆 路由
 Route::get('admin/login','Admin\LoginController@login');
+
+// 后台 退 出 路由
+Route::get('admin/logout','Admin\LoginController@logout');
+
 // 后台 登陆验证路由
 Route::post('admin/dologin','Admin\LoginController@dologin');
+
+
 // 后台 登陆中间件
 Route::group(['middleware'=>'login'],function(){
 
+   // 进入 后台页面
+   Route::get('admin','Admin\IndexController@index');
 
-// 后台 修改 用户状态 路由
-Route::get('admin/users/status','Admin\UsersController@status');
+   // 商品推荐 路由
+   Route::get('admin/goods/setRecommend/{id}','Admin\GoodsController@setRecommend');
 
-//  后台用户 路由
-Route::resource('admin/users','Admin\UsersController');
+   // 商品大小显示页面 路由
+   Route::get('admin/sizes/create/{id}','Admin\SizesController@create');
 
-// 后台 底部 路由
-Route::resource('admin/footer','Admin\FooterController');
+   // 商品大小执行添加 路由
+   Route::post('admin/sizes/store','Admin\SizesController@store');
+
+   // 后台商品属性 添加页面 路由
+   Route::get('admin/attributes/create/{id}','Admin\AttributesController@create');
+
+   // 后台商品属性 执行添加 路由
+   Route::post('admin/attributes/store/{id}','Admin\AttributesController@store');
+
+   // 后台用户留言列表 显示 路由
+   Route::get('admin/replys/index','Admin\ReplysController@index');
+
+
+   // 后台查看用户留言详情 路由
+   Route::get('admin/replys/{id}','Admin\ReplysController@show');
+
+   // 后台查看用户留言详情 路由
+   Route::get('admin/replys','Admin\ReplysController@index');
+
+   // 后台 修改 管理员密码 路由
+   Route::post('admin/users/update_upass/{id}','Admin\UsersController@update_upass');
+   // 后台 修改 管理员 头像 路由
+   Route::post('admin/users/update_profile/{id}','Admin\UsersController@update_profile');
+
+   // 后台 修改 用户状态 路由
+   Route::get('admin/users/status/{id}','Admin\UsersController@status');
+
+   //  后台用户 路由
+   Route::resource('admin/users','Admin\UsersController');
+
+   // 后台 底部 路由
+   Route::resource('admin/footer','Admin\FooterController');
+
+
+   
+
+   // 友情链接
+   Route::resource('admin/links','Admin\LinksController');
+
+   // 导航
+   Route::resource('admin/navicates','Admin\NavigatesController');
+
+   // 分类
+   Route::resource('admin/cates','Admin\CatesController');
+
+   // 品牌
+   Route::resource('admin/brands','Admin\BrandsController');
+
+
+   // 订单
+   Route::resource('admin/orders','Admin\OrdersController');
+
+
+   // 公告列表 路由
+   Route::resource('admin/news','Admin\NewsController');
+
+   // 商品列表 路由
+   Route::resource('admin/goods','Admin\GoodsController');
+
+
+
+   // 商品型号添加 路由
+   Route::resource('admin/models','Admin\ModelsController');
+
+   //轮播图 路由
+   Route::resource('admin/banners','Admin\BannersController');
+
+
+
 
 });
 
@@ -306,8 +364,7 @@ Route::resource('admin/footer','Admin\FooterController');
 
 
 
-// 进入 后台页面
-Route::resource('admin','Admin\IndexController');
+
 
 Route::resource('/','Home\IndexController');
 
